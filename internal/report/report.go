5ee4a0a7d739487e2923c700ca1b33c603fb561b
Nathan ter Bogt <nterbogt@gmail.com>
2025-09-08 09:42:41 +1200

    Non-cluster types (#2)
    * Adding other non-cluster report types
    
    * Newlines at end of files
    
    * Adding the secret sorter for exposed secrets

--- internal/report/report.go
+++ internal/report/report.go
@@ -9,16 +9,22 @@ import (
 
 type Report struct {
 	ClusterConfigAudits    []*v1alpha1.ClusterConfigAuditReport
-	ConfigAudits           []*v1alpha1.ConfigAuditReport
 	ClusterVulnerabilities []*v1alpha1.ClusterVulnerabilityReport
+	ConfigAudits           []*v1alpha1.ConfigAuditReport
+	ExposedSecrets         []*v1alpha1.ExposedSecretReport
+	InfraAssessments       []*v1alpha1.InfraAssessmentReport
+	RbacAssessments        []*v1alpha1.RbacAssessmentReport
 	Vulnerabilities        []*v1alpha1.VulnerabilityReport
 }
 
 func NewReport() Report {
 	return Report{
 		ClusterConfigAudits:    []*v1alpha1.ClusterConfigAuditReport{},
-		ConfigAudits:           []*v1alpha1.ConfigAuditReport{},
 		ClusterVulnerabilities: []*v1alpha1.ClusterVulnerabilityReport{},
+		ConfigAudits:           []*v1alpha1.ConfigAuditReport{},
+		ExposedSecrets:         []*v1alpha1.ExposedSecretReport{},
+		InfraAssessments:       []*v1alpha1.InfraAssessmentReport{},
+		RbacAssessments:        []*v1alpha1.RbacAssessmentReport{},
 		Vulnerabilities:        []*v1alpha1.VulnerabilityReport{},
 	}
 }
@@ -33,6 +39,16 @@ func (r *Report) AppendClusterConfigAuditReports(reports v1alpha1.ClusterConfigA
 	}
 }
 
+func (r *Report) AppendClusterVulnerabilityReports(reports v1alpha1.ClusterVulnerabilityReportList) {
+	for _, vulnerabilityReport := range reports.Items {
+		if len(vulnerabilityReport.Report.Vulnerabilities) == 0 {
+			continue
+		}
+		sort.Sort(sorter.VulnerabilitySorter(vulnerabilityReport.Report.Vulnerabilities))
+		r.ClusterVulnerabilities = append(r.ClusterVulnerabilities, &vulnerabilityReport)
+	}
+}
+
 func (r *Report) AppendConfigAuditReports(reports v1alpha1.ConfigAuditReportList) {
 	for _, configAuditReport := range reports.Items {
 		if len(configAuditReport.Report.Checks) == 0 {
@@ -43,13 +59,33 @@ func (r *Report) AppendConfigAuditReports(reports v1alpha1.ConfigAuditReportList
 	}
 }
 
-func (r *Report) AppendClusterVulnerabilityReports(reports v1alpha1.ClusterVulnerabilityReportList) {
-	for _, vulnerabilityReport := range reports.Items {
-		if len(vulnerabilityReport.Report.Vulnerabilities) == 0 {
+func (r *Report) AppendExposedSecretReports(reports v1alpha1.ExposedSecretReportList) {
+	for _, exposedSecretReport := range reports.Items {
+		if len(exposedSecretReport.Report.Secrets) == 0 {
 			continue
 		}
-		sort.Sort(sorter.VulnerabilitySorter(vulnerabilityReport.Report.Vulnerabilities))
-		r.ClusterVulnerabilities = append(r.ClusterVulnerabilities, &vulnerabilityReport)
+		sort.Sort(sorter.SecretSorter(exposedSecretReport.Report.Secrets))
+		r.ExposedSecrets = append(r.ExposedSecrets, &exposedSecretReport)
+	}
+}
+
+func (r *Report) AppendInfraAssessmentReports(reports v1alpha1.InfraAssessmentReportList) {
+	for _, infraAssessmentReport := range reports.Items {
+		if len(infraAssessmentReport.Report.Checks) == 0 {
+			continue
+		}
+		sort.Sort(sorter.CheckSorter(infraAssessmentReport.Report.Checks))
+		r.InfraAssessments = append(r.InfraAssessments, &infraAssessmentReport)
+	}
+}
+
+func (r *Report) AppendRbacAssessmentReports(reports v1alpha1.RbacAssessmentReportList) {
+	for _, rbacAssessmentReport := range reports.Items {
+		if len(rbacAssessmentReport.Report.Checks) == 0 {
+			continue
+		}
+		sort.Sort(sorter.CheckSorter(rbacAssessmentReport.Report.Checks))
+		r.RbacAssessments = append(r.RbacAssessments, &rbacAssessmentReport)
 	}
 }
 
